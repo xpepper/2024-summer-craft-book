91ba7fd23b32a7ea643ee0c914b10e2171cbb274
Pietro Di Bello <pierodibello@gmail.com>
2024-08-07 23:06:49 +0200

    refactor: convert to when

--- exercise/kotlin/week02-exercise1/src/main/kotlin/games/FizzBuzz.kt
+++ exercise/kotlin/week02-exercise1/src/main/kotlin/games/FizzBuzz.kt
@@ -6,17 +6,13 @@ object FizzBuzz {
             throw OutOfRangeException()
         }
 
-        if (input % 3 == 0 && input % 5 == 0) {
-            return "FizzBuzz"
-        }
-        if (input % 3 == 0) {
-            return "Fizz"
-        }
-        if (input % 5 == 0) {
-            return "Buzz"
-        }
+        return when {
+            input % 3 == 0 && input % 5 == 0 -> "FizzBuzz"
+            input % 3 == 0 -> "Fizz"
+            input % 5 == 0 -> "Buzz"
 
-        return input.toString()
+            else -> input.toString()
+        }
     }
 }
 
